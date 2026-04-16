3e7dc034a1ceb29ba202862fcefccbdd53db9a85
Dhanvish <sjdhanvish@gmail.com>
2026-04-16 19:52:44 +0530

    changed url to uri

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+    connectionString: process.env.POSTGRES_URI,
   ssl: {
     rejectUnauthorized: false,
   },
